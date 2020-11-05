dc1896fbbda0bd1a394e473ed652f3e17c17871b
anapy <anabelenfunesc@gmail.com>
2020-11-05 14:22:21 +0100

    display messages

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -12,8 +13,15 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+    ) {}
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+
   }
 
   getHeroes(): void {
@@ -21,9 +29,6 @@ export class HeroesComponent implements OnInit {
     .subscribe(heroes => this.heroes = heroes);
   }
 
-  constructor(
-    private heroService: HeroService
-    ) {}
 
   ngOnInit() {
     this.getHeroes();
